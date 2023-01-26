dbf14012c879337a622d53dd95f5a794f1c7dbf6
fuzhong2020 <62815355+fuzhong2020@users.noreply.github.com>
2023-01-26 08:20:50 +0100

    final abgabe

---
+++ abgabe/code/engineering.py
@@ -0,0 +1,82 @@
+"""
+Module performs data preprocessing and feature engineering.
+"""
+import os
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+def add_ageincome(df_edit: pd.DataFrame) -> pd.DataFrame:
+    """
+    creates column 'ageincome',
+    calculated by dividing column "age" by the average of column "annual_income".
+
+    :param pd.DataFrame df_edit: dataframe 
+    :return: the edited dataframe
+    """
+    df_edit["ageincome"] = df_edit["Age"] / df_edit["annual_income"].mean()
+    return df_edit
+
+
+def rename_column(df_edit: pd.DataFrame, column_names: list[str]) -> pd.DataFrame:
+    """
+    renames column 'old_name' to 'new_name'
+
+    :param pd.DataFrame df_edit: dataframe
+    :param list[str] column_names: old column name, new column name
+    :return: the edited dataframe
+    """
+
+    for names in column_names:
+        df_edit = df_edit.rename(columns={names[0]: names[1]})
+    return df_edit
+
+
+def build_df(path: str) -> pd.DataFrame:
+    """
+    builds data frame with all feature engineering methods, renames columns, creates new column
+
+    :param tupel[list[str]] paths: list containing the path to all csv files
+    :return: df containing the data of all csv-files
+    """
+
+    name_changes = [("Annual Income (k$)", "annual_income"), ("Spending Score (1-100)", "spending_score")]
+
+    return normalize(add_ageincome(
+        rename_column(pd.read_csv(path), name_changes).drop(["CustomerID"], axis=1))
+            )
+
+def convert_column_to_num(column: pd.Series) -> pd.Series:
+    """
+    converts values of column to numerical values using integers encoding method
+
+    :param pd.Series column: column to be converted
+    :return: converted column
+    """
+    return column.replace(column.unique(), range(1, len(column.unique()) + 1))
+
+
+
+def normalize(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    normalizes data in order to get a smaller scale of values, utilizes numpy
+
+    :param df: the dataframe to be normalized
+    :return: the normalized dataframe
+    """
+    df_norm = df.copy()
+    scaler = MinMaxScaler()
+    df_norm[["ageincome", "spending_score"]] = scaler.fit_transform(df_norm[["ageincome", "spending_score"]])
+
+    return df_norm
+
+
+def main():
+    csv_path = 'data\Mall_Customers.csv'
+
+    df = build_df(csv_path) #build new csv
+    df.to_csv('data/out/clean_mall_customer.csv', index=False)  # put df in folder out
+
+
+if __name__ == "__main__":
+    main()
---
+++ abgabe/code/train.py
@@ -0,0 +1,86 @@
+import mlflow
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score, silhouette_score
+from sklearn.model_selection import train_test_split
+
+
+def import_df() -> pd.DataFrame:
+    """
+    imports dataset as pandas dataframe
+
+    :return: pandas dataframe
+    """
+    df_imp = pd.read_csv('data\out\clean_mall_customer.csv')
+    return df_imp
+
+
+def metrics(data: pd.DataFrame, labels: np.ndarray) -> list[float]:
+    """
+    calculates metrics silhouette, calinski and davies for clustering
+
+    :param data: validation dataset
+    :param labels: the clustering labels of the data in X
+    :return: returns metrics for clustering
+    """
+    return [silhouette_score(data, labels),
+            calinski_harabasz_score(data, labels),
+            davies_bouldin_score(data, labels)]
+
+
+def split_df(data: pd.DataFrame) -> list[pd.DataFrame]:
+    """
+    splits the dataframe into 3 dataframes at random, with distribution:
+    train: 70%, validation: 20%, test: 10%
+    
+    :param data: X of model
+    :return: X for train, test and validation
+    """
+    X_train, X_test = train_test_split(data, test_size=0.3, random_state=42)
+
+    # get 10% test data
+    X_valid, X_test = train_test_split(X_test, test_size=1/3, random_state=42)
+
+    return [X_train, X_test, X_valid]
+
+
+
+def train(X: pd.DataFrame, X_val: pd.DataFrame, n_clusters: int = 10):
+    """
+    trains a clustering model using columns "ageincome" and "spending_scpre". 
+    logs the metrics and params with mlfow for replicability.
+    :param X: training dataset
+    :param X_val: validation dataset
+    :param n_clusters: number of clusters
+    """
+    
+    with mlflow.start_run():
+        model = KMeans(n_clusters=n_clusters, random_state=69)
+        model.fit(X)
+        predict = model.predict(X_val)
+
+        silhouette, calinski, davies = metrics(X_val, predict)
+
+
+        mlflow.log_param("n_clusters", n_clusters)
+        mlflow.log_metric("Silhouette", silhouette)
+        mlflow.log_metric("Calinski-Harabasz", calinski)
+        mlflow.log_metric("Davies-Bouldin", davies)
+
+        mlflow.sklearn.log_model(model, "model")
+
+
+
+
+def main():
+    df = import_df()
+    X_train, X_test, X_val = split_df(data = df[['ageincome','spending_score']])
+
+    for i in range(20, 1, -1):
+        train(X=X_train, X_val=X_val, n_clusters=i)
+
+
+
+if __name__ == "__main__":
+    main()
